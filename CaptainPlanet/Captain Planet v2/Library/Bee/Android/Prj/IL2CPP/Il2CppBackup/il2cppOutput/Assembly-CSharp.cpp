09c4422efef11953a5dc6579b31a4cc6bdc44e8c
Muhammad Qorano Wanggai <s222016651@deakin.edu.au>
2023-05-01 01:46:41 +1000

    Added initial project files with Android mobile platform project configuration and ARCore marking prototype

---
+++ CaptainPlanet/Captain Planet v2/Library/Bee/Android/Prj/IL2CPP/Il2CppBackup/il2cppOutput/Assembly-CSharp.cpp
@@ -0,0 +1,168 @@
+﻿#include "pch-cpp.hpp"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include <limits>
+
+
+
+// ElementalPlacement
+struct ElementalPlacement_tEF16E84CC9D17189272A7B0258C43C0833FE5047;
+// UnityEngine.MonoBehaviour
+struct MonoBehaviour_t532A11E69716D348D8AA7F854AFCBFCB8AD17F71;
+// System.Void
+struct Void_t4861ACF8F4594C3437BB48B6E56783494B843915;
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// <Module>
+struct U3CModuleU3E_tBB65183F1134474D09FF49B95625D25472B9BA8B 
+{
+};
+
+// System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F  : public RuntimeObject
+{
+};
+// Native definition for P/Invoke marshalling of System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_pinvoke
+{
+};
+// Native definition for COM marshalling of System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_com
+{
+};
+
+// System.IntPtr
+struct IntPtr_t 
+{
+	// System.Void* System.IntPtr::m_value
+	void* ___m_value_0;
+};
+
+// System.Void
+struct Void_t4861ACF8F4594C3437BB48B6E56783494B843915 
+{
+	union
+	{
+		struct
+		{
+		};
+		uint8_t Void_t4861ACF8F4594C3437BB48B6E56783494B843915__padding[1];
+	};
+};
+
+// UnityEngine.Object
+struct Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C  : public RuntimeObject
+{
+	// System.IntPtr UnityEngine.Object::m_CachedPtr
+	intptr_t ___m_CachedPtr_0;
+};
+// Native definition for P/Invoke marshalling of UnityEngine.Object
+struct Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C_marshaled_pinvoke
+{
+	intptr_t ___m_CachedPtr_0;
+};
+// Native definition for COM marshalling of UnityEngine.Object
+struct Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C_marshaled_com
+{
+	intptr_t ___m_CachedPtr_0;
+};
+
+// UnityEngine.Component
+struct Component_t39FBE53E5EFCF4409111FB22C15FF73717632EC3  : public Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C
+{
+};
+
+// UnityEngine.Behaviour
+struct Behaviour_t01970CFBBA658497AE30F311C447DB0440BAB7FA  : public Component_t39FBE53E5EFCF4409111FB22C15FF73717632EC3
+{
+};
+
+// UnityEngine.MonoBehaviour
+struct MonoBehaviour_t532A11E69716D348D8AA7F854AFCBFCB8AD17F71  : public Behaviour_t01970CFBBA658497AE30F311C447DB0440BAB7FA
+{
+};
+
+// ElementalPlacement
+struct ElementalPlacement_tEF16E84CC9D17189272A7B0258C43C0833FE5047  : public MonoBehaviour_t532A11E69716D348D8AA7F854AFCBFCB8AD17F71
+{
+};
+
+// <Module>
+
+// <Module>
+
+// System.Void
+
+// System.Void
+
+// UnityEngine.MonoBehaviour
+
+// UnityEngine.MonoBehaviour
+
+// ElementalPlacement
+
+// ElementalPlacement
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+
+
+// System.Void UnityEngine.MonoBehaviour::.ctor()
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void MonoBehaviour__ctor_m592DB0105CA0BC97AA1C5F4AD27B12D68A3B7C1E (MonoBehaviour_t532A11E69716D348D8AA7F854AFCBFCB8AD17F71* __this, const RuntimeMethod* method) ;
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+// System.Void ElementalPlacement::Start()
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void ElementalPlacement_Start_m85646D5A5AA338F4ADFEAAD612C39E22334346D0 (ElementalPlacement_tEF16E84CC9D17189272A7B0258C43C0833FE5047* __this, const RuntimeMethod* method) 
+{
+	{
+		// }
+		return;
+	}
+}
+// System.Void ElementalPlacement::Update()
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void ElementalPlacement_Update_mC2CF32F0F4DF533C73FC166667876F39F752CD4E (ElementalPlacement_tEF16E84CC9D17189272A7B0258C43C0833FE5047* __this, const RuntimeMethod* method) 
+{
+	{
+		// }
+		return;
+	}
+}
+// System.Void ElementalPlacement::.ctor()
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void ElementalPlacement__ctor_m3D889938DB8C3D5C46083990A241709B42048EDA (ElementalPlacement_tEF16E84CC9D17189272A7B0258C43C0833FE5047* __this, const RuntimeMethod* method) 
+{
+	{
+		MonoBehaviour__ctor_m592DB0105CA0BC97AA1C5F4AD27B12D68A3B7C1E(__this, NULL);
+		return;
+	}
+}
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
---
+++ CaptainPlanet/Captain Planet v2/Library/Bee/artifacts/Android/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,53 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ElementalPlacement::Start()
+extern void ElementalPlacement_Start_m85646D5A5AA338F4ADFEAAD612C39E22334346D0 (void);
+// 0x00000002 System.Void ElementalPlacement::Update()
+extern void ElementalPlacement_Update_mC2CF32F0F4DF533C73FC166667876F39F752CD4E (void);
+// 0x00000003 System.Void ElementalPlacement::.ctor()
+extern void ElementalPlacement__ctor_m3D889938DB8C3D5C46083990A241709B42048EDA (void);
+static Il2CppMethodPointer s_methodPointers[3] = 
+{
+	ElementalPlacement_Start_m85646D5A5AA338F4ADFEAAD612C39E22334346D0,
+	ElementalPlacement_Update_mC2CF32F0F4DF533C73FC166667876F39F752CD4E,
+	ElementalPlacement__ctor_m3D889938DB8C3D5C46083990A241709B42048EDA,
+};
+static const int32_t s_InvokerIndices[3] = 
+{
+	2067,
+	2067,
+	2067,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	3,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
